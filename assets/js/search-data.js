9e2ca03e746aeb1a43f6cf76822cb659a1395ff7
tugrulhkarabulut <tugrulkarabulut64@gmail.com>
2025-08-31 03:06:25 +0000

    Deploying to gh-pages from @ tugrulhkarabulut/tugrulhkarabulut.github.io@5c5f3c3f4fd71d315f20b1d93a1bcbfe216a0cdc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -440,16 +440,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-graduated-from-the-computer-engineering-master-s-program-at-boğaziçi-university",
+          title: 'I graduated from the computer engineering master’s program at Boğaziçi University!',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-i-started-my-phd-program-in-university-of-michigan",
+          title: 'I started my PhD program in University of Michigan!',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-our-new-preprint-is-posted-local-virtual-nodes-for-alleviating-over-squashing-in-gnns",
+          title: 'Our new preprint is posted: Local Virtual Nodes for Alleviating Over-Squashing in GNNs...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
